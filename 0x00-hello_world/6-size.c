57074a029e79514f445351199c077b4c30466bc3
sayed <saidmo19734@gmail.com>
2023-03-16 12:54:16 -0700

    intial commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %u byte(s)", sizeof(char));
-	printf("Size of an int: %u byte(s)", sizeof(int));
-	printf("Size of a long int: %u byte(s)", sizeof(long int));
-	printf("Size of a long long int: %u byte(s)", sizeof(long long int));
-	printf("Size of a float: %u byte(s)", sizeof(float));
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of an int: %lu byte(s)", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
 	return (0);
 }
