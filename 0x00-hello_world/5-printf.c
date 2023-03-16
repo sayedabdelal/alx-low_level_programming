fdc75359330d3f4aa8c5513d34da60b88ae593cd
sayed <saidmo19734@gmail.com>
2023-03-16 10:21:28 -0700

    intial commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include < stdio.h >
+
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program prints with printf funtion
+ *
+ * Return: Always 0 (Sucsses)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
