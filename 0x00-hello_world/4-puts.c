293a3020098e99b55037d91df71c39c0ad73afc8
sayed <saidmo19734@gmail.com>
2023-03-16 10:12:46 -0700

    intilal commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description : A c programming that prints wiht puts function
+ *
+ * Return: Always  0 (succcess)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
