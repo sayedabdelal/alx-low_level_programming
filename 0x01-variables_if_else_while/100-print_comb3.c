14dd4b7bd6bacbca689fcaa5796adbe7128ceb57
sayed <saidmo19734@gmail.com>
2023-03-19 08:36:35 -0700

    intial commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description : print  all possible different combinations of two digits.
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int one = 1;
+	int two = 0;
+
+	while (two < 9)
+	{
+		one = 0;
+		while (one <= 9)
+		{
+			if (one != two && two < one)
+			{
+				putchar(two + 48);
+				putchar(one + 48);
+				if (one + two != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			one++;
+		}
+		two++;
+	}
+	putchar('\n');
+
+}
