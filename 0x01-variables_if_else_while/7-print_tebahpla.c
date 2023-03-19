9eec7c5d41f30f29a2bfe229a2728e55b95b3db0
sayed <saidmo19734@gmail.com>
2023-03-19 06:18:13 -0700

    intial commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description : print all revers alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
