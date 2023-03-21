facf2fe4a4a3fd85c8f6261144b037c13a95ddcb
sayed <saidmo19734@gmail.com>
2023-03-21 12:28:45 -0700

    intial commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - function computes the absolute value of an integer
+ *
+ * @n: parameter to transport a number to absoulte
+ *
+ * Return: Always 0 (success)
+*/
+
+int _abs(int n)
+{
+	int ab = abs(n);
+
+	return (ab);
+}
