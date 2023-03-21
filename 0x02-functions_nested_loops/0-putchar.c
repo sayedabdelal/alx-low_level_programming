e4d813de473f1ba3a3b4b14afa255ec2d24f1ea8
sayed <saidmo19734@gmail.com>
2023-03-21 06:36:13 -0700

    intial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description : print word >> _putchar
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
