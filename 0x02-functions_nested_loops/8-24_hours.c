2627ac5aa75ea5e1b3bf6a33ad09678a083b2b56
sayed <saidmo19734@gmail.com>
2023-03-21 13:54:13 -0700

    intial commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer- function prints the very minute of the day
+ *
+ * Return: Always 0 (success)
+*/
+
+void jack_bauer(void)
+{
+	int i, j;
+	int n1, n2, n3, n4;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			n1 = i / 10;
+			n2 = i % 10;
+			n3 = j / 10;
+			n4 = j % 10;
+			_putchar(n1 + '0');
+			_putchar(n2 + '0');
+			_putchar(':');
+			_putchar(n3 + '0');
+			_putchar(n4 + '0');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
