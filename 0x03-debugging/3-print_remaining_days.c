8a488066b7dff244f098d6551f3158604bdd479f
sayed <saidmo19734@gmail.com>
2023-03-22 10:11:56 -0700

    intial commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,11 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || !(year % 100 == 0))
+	/**
+	 * leap your when it can be divided
+	 * by 100 and 400 or 4 evenly
+	 */ 
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 100 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
