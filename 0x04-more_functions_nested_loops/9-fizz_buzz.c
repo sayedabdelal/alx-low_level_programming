86acd76423d0196b359bab3faa88e22b9aadf8a4
sayed <saidmo19734@gmail.com>
2023-03-23 13:11:18 -0700

    intial commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,25 @@ int main(void)
 	for (i = 1; i <= 100 ; i++)
 	{
 		if (i % 5 == 0 && i % 3 == 0)
+		{
 			printf(" FizzBuzz");
-		else if (i % 5 == 0  && i % 3 != 0)
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
 			printf(" Buzz");
+		}
 		else if (i % 3 == 0 && i % 5 != 0)
+		{
 			printf(" Fizz");
+		}
 		else if (i == 1)
+		{
 			printf("%d", i);
+		}
 		else
-			printf("%d ", i);
+		{
+			printf(" %d", i);
+		}
 	}
 	printf("\n");
 	return (0);
