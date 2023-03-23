60e337188a0c7f50f8ae9fbe472d23421b23e258
sayed <saidmo19734@gmail.com>
2023-03-23 07:20:59 -0700

    intial commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,11 +9,14 @@ void print_line(int n)
 {
 	int i;
 
-	if (n > 0)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 1; i <= n; i++)
+		{
 			_putchar('_');
+		}
 		_putchar('\n');
-	} else
-		_putchar('\n');
+	}
 }
