8074d852c2370102faebc9504e2af7984b4934ae
sayed <saidmo19734@gmail.com>
2023-03-27 17:04:14 -0700

    intial commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string.
+ *@str: string to use.
+ */
+
+void puts_half(char *str)
+{
+	int length;
+
+	for (length = 0; str[length] != '\0'; length++)
+		;
+	length++;
+	for (length /= 2; str[length] != '\0'; length++)
+	{
+		_putchar(str[length]);
+	}
+	_putchar('\n');
+}	
