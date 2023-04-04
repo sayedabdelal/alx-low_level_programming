c949740772c3cc60aef1307056a8567b273590f0
sayed <saidmo19734@gmail.com>
2023-04-04 12:55:41 -0700

    int

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
  * _strchr - searches for the first occurrence of the character
  * @s: pointer to char
  * @c: char params to found
- * Return: *S + id pointer
+ * Return: *S
 */
 
 char *_strchr(char *s, char c)
