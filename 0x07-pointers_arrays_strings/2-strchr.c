030721c14aadfd0096cc678255a2ce8d9e8b4cbb
sayed <saidmo19734@gmail.com>
2023-04-04 10:40:51 -0700

    intiola commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - searches for the first occurrence of the character
+ * @s: pointer to char
+ * @c: char params to found
+ * Return: *S + id pointer
+*/
+
+char *_strchr(char *s, char c)
+{
+	int id;
+
+	for (id = 0; id != '\0'; id++)
+	{
+		if (s[id] == c)
+		{
+			return (s + id);
+		}
+	}
+	return ('\0');
+}
