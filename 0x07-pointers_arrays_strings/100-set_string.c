666a424ae81305422fae163246bf5e665e0bcb7d
sayed <saidmo19734@gmail.com>
2023-04-04 14:33:55 -0700

    intila commit

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,7 +2,7 @@
 
 /**
  * set_string - sets the value of a pointer to a char
- * @s: pointer to pointer we need to 
+ * @s: pointer to pointer we need to
  * @to: string to set
  */
 void set_string(char **s, char *to)
