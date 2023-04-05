54d2f2d7224471373b6f8f22889aab155f6f5056
sayed <saidmo19734@gmail.com>
2023-04-05 08:08:13 -0700

    intila commit

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compare strings and return one
+ * @s1: pointer to string parameter
+ * @s2: pointer to string paramter
+ * Return: 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '\0' && *s1 == '\0')
+		return (1);
+
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+
+	return (0);
+}
