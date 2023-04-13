b1e730f4771dcaf42f745d351c0e14f3abc85708
sayed <saidmo19734@gmail.com>
2023-04-13 06:59:29 -0700

    inti

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,31 +1,63 @@
 #include <stdlib.h>
 #include "main.h"
-
+#include <string.h>
 /**
- * *_realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previsouly allocated by malloc
- * @old_size: size of the allocated memory for ptr
- * @new_size: new size of the new memory block
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated with malloc
+ * @old_size: size of the allocated space for ptr
+ * @new_size: new size of the memory block
  *
- * Return: pointer to the newly allocated memory block
+ * Return: pointer to the new memory block, or NULL if new_size is 0 and ptr is not NULL,
+ * or NULL if the function fails to allocate the requested memory
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	if (new_size == old_size)
-		return (ptr);
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	if (ptr == NULL)
-	{
-		ptr = malloc(new_size);
-	}
-	free(ptr);
+    void *new_ptr;
 
-	ptr = malloc(new_size);
+    /* If new_size is 0, free the memory block pointed to by ptr and return NULL */
+    if (new_size == 0 && ptr != NULL)
+    {
+        free(ptr);
+        return NULL;
+    }
 
-	return (ptr);
+    /* If ptr is NULL, allocate a new memory block of size new_size and return a pointer to it */
+    if (ptr == NULL)
+    {
+        new_ptr = malloc(new_size);
+        if (new_ptr == NULL)
+        {
+            return NULL;
+        }
+        return new_ptr;
+    }
 
+    /* If new_size is equal to old_size, return ptr */
+    if (new_size == old_size)
+    {
+        return ptr;
+    }
+
+    /* Allocate a new memory block of size new_size */
+    new_ptr = malloc(new_size);
+    if (new_ptr == NULL)
+    {
+        return NULL;
+    }
+
+    /* Copy the contents of the old memory block to the new memory block */
+    if (new_size > old_size)
+    {
+        memcpy(new_ptr, ptr, old_size);
+    }
+    else
+    {
+        memcpy(new_ptr, ptr, new_size);
+    }
+
+    /* Free the old memory block */
+    free(ptr);
+
+    /* Return a pointer to the new memory block */
+    return new_ptr;
 }
