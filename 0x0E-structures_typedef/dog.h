66923c57d85adc9ee0e508bf25017fad2ec926de
sayed <saidmo19734@gmail.com>
2023-04-18 11:43:15 -0700

    int

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a  Dog attributes
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner:  owner of the dog.
+ *
+ * Description: dog struct in big kitty world
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
