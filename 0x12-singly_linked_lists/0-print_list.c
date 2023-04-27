75d55533b4510ce1171bf940e0974c2ea74cb8e6
sayedabdelal <saidmo19734@gmail.com>
2023-04-27 14:58:06 -0700

    intial commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * _strlen - calc length of string
+ * @s: check string
+ * Return: int of len
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	for (i = 0; *s != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ * print_list - print a linked list
+ * @h: pointer to frist code
+ *
+ * Return: num of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t j = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		j++;
+	}
+	return (j);
+}
