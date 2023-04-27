90a8ccfc195a6aa69a88f441d2778045c8bd3ab0
sayedabdelal <saidmo19734@gmail.com>
2023-04-27 15:13:51 -0700

    commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,28 +8,26 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
+	list_t *new_node;
 
-    if (head == NULL || str == NULL)
-        return (NULL);
+	if (head == NULL || str == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->len = 0;
-    while (new_node->str[new_node->len] != '\0')
-        new_node->len++;
-
-    new_node->next = *head;
-    *head = new_node;
-
-    return (new_node);
+	new_node->len = 0;
+	while (new_node->str[new_node->len] != '\0')
+		new_node->len++;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
