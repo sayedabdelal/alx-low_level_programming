24c2389010d73f84cc9bfe338f4abfa19f59a986
sayedabdelal <saidmo19734@gmail.com>
2023-04-28 06:28:26 -0700

    intial commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -28,5 +28,6 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _putchar(char c);
 int _strlen(char *s);
+void print_before_main(void) __attribute__ ((constructor));
 
 #endif
