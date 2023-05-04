f050192ead5fc0e4ef96f411eefa4051daec9750
sayedabdelal <saidmo19734@gmail.com>
2023-05-04 15:04:09 -0700

    intilal commit

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_endianness - return  the endianness of system
+ * Return: 0 if big endian, 1 if small endian
+ */
+int get_endianness(void)
+{
+	unsigned long int i = 1;
+
+	return (*(char *)&n);
+}
