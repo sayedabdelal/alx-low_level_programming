9fbe1937d9d4e5ee9b81eb7173a6a9962dc466cf
sayedabdelal <saidmo19734@gmail.com>
2023-05-09 09:19:04 -0700

    int

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: number of bytes read/priented
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t b_file;
+	char buf[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+	fd = open(filename, O_RDONLY)
+	if (fd == -1)
+		return (0);
+	b_file = read(fd, &buf[0], letters);
+	b_file = write(STDPUT_FILENO, &buf[0], bytes);
+	close(fd);
+	return (b_file);
+}
