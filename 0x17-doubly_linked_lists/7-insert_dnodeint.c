097ea07085e5ae89a1d0de5bb80b50cc5cbbd7e4
sayedabdelal <saidmo19734@gmail.com>
2023-06-17 03:56:34 -0700

    chore

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Add node at spesfic postion
+ *
+ * @h: Head
+ *
+ * @idx: index
+ *
+ * @n: struct int
+ *
+ * Return: dlistint_t
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newP, *tmp, *tmp2;
+
+	newP = malloc(sizeof(dlistint_t));
+	if (newP == NULL)
+	{
+		return (NULL); /*Check if memory allocation failed */
+	}
+	newP->n = n;
+	newP->next = NULL;
+	newP->prev = NULL;
+	if (idx == 0)
+	{
+		newP->next = *h;
+		if (*h != NULL)
+		{
+			(*h)->prev = newP;
+		}
+		*h = newP;
+		return (newP);
+	}
+	tmp = *h;
+	while (idx != 1 && tmp != NULL)
+	{
+		tmp = tmp->next;
+		idx--;
+	}
+	if (tmp == NULL)
+	{
+		free(newP);
+		return (NULL);
+	}
+	tmp2 = tmp->next;
+	tmp->next = newP;
+	tmp2->prev = newP;
+	newP->next = tmp2;
+	newP->prev = tmp;
+	return (newP);
+}
