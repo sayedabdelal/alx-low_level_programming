fa4c460a8297cfaf2be22ae9c807c8bc44a168ba
sayedabdelal <saidmo19734@gmail.com>
2023-06-20 13:03:25 -0700

    fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#!/bin/bash
 wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="$PWD/../liba.so"
