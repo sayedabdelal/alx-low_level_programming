ea013100a22f0659fa2385364d0f117cca8daf9a
sayedabdelal <saidmo19734@gmail.com>
2023-07-10 02:11:09 -0700

    chore

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(tmp->key, key) == 0)
 		{
 			free(tmp->value);
-			temp->value = strdup(value);
+			tmp->value = strdup(value);
 			if (tmp->value == NULL)
 				return (0);
 			return (1);
