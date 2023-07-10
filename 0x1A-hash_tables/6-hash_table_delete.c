93d0e406751599ef486e4093da0f33f8870d3bea
sayedabdelal <saidmo19734@gmail.com>
2023-07-10 02:23:28 -0700

    chore

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - remove a hash table
+ * @ht: hash table to delete
+ *
+ * Return: Nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *noode, *tmp;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		noode = ht->array[i];
+		while (noode != NULL)
+		{
+			tmp = noode;
+			node = noode->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
